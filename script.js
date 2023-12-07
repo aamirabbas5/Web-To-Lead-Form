9ac34107b6ce6329d6bf487aeb742f9f3cc5bcdc
Aamir Abbas <aamirabbas.sf@gmail.com>
2023-12-07 18:07:37 +0500

    reCHAPTCHA added

--- script.js
+++ script.js
@@ -7,7 +7,26 @@ function beforesubmit() {
     outputdate.value = formatedDate;
 }
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") { var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
         elems["ts"] = JSON.stringify(new Date().getTime());
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } }
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+    }
+}
 setInterval(timestamp, 500);
+
+function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Check if reCAPTCHA is checked
+    var response = grecaptcha.getResponse();
+
+    if (response.length === 0) {
+        alert("Please check reCAPTCHA before submitting the form!");
+    } else {
+        // If reCAPTCHA is checked, submit the form
+        document.getElementById("myForm").submit();
+    }
+}
